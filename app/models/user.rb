8fcb949daa186034365adce485677a362c5226fe
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-04 11:54:58 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :name            , presence: true
-  validates :family_name     , presence: true
-  validates :first_name      , presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana , presence: true
-  validates :birth_day       , presence: true
+  
+  with_options presence: true do
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+      validates :name            , presence: true
+      validates :family_name     , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+      validates :first_name      , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+      validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+      validates :first_name_kana , presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+      validates :birth_day       , presence: true
+  end
 end
