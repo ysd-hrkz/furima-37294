61b89f962dc0899cbdd8a8f07375a3d55e7cd092
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-04 12:51:18 +0900

    カラム名修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname { Faker::Name.name }
+    name { Faker::Name.name }
     email { Faker::Internet.unique.email }
     # ランダムで生成する際、英数字になるよう'1a'追加
     password { '1a' + Faker::Internet.unique.password(min_length: 6) }
     password_confirmation { password }
-    last_name { '山田' }
+    family_name { '山田' }
     first_name { '太郎' }
-    last_name_kana { 'ヤマダ' }
+    family_name_kana { 'ヤマダ' }
     first_name_kana { 'タロウ' }
     birthday { Faker::Date.birthday }
   end
