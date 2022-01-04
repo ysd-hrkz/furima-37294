813e22f59236c307c75268d4cf748f544a11b523
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-04 18:02:43 +0900

    itemテーブルとActiveStorageのアソシエーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+  def message_params
+    params.require(:item).permit(:image, :name, :price, :description, :condition_id, :category_id, :shipping_area_id, :shipping_fee_id, :shipping_days_id).merge(user_id: current_user.id)
+  end
 end
