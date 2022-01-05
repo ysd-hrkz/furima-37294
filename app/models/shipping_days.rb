02d52a21612324931f68b7f1e448f630c15edf79
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-05 12:15:46 +0900

    各ActiveHash設定

--- app/models/shipping_days.rb
+++ app/models/shipping_days.rb
@@ -1,3 +1,11 @@
 class Shipping_days < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---'},
+    { id: 2, name: '3日で発送'},
+    { id: 3, name: '4〜7日で発送'},
+    { id: 4, name: '目立った傷や汚れなし'}
+  ]
+
+  include ActiveHash::Associations
   has_many :items
 end
