2a98545a33479af93f0562d8ea9dbc59d82aad0d
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-05 11:41:05 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+
   belongs_to :user
 
   has_one_attached :image
@@ -9,4 +10,17 @@ class Item < ApplicationRecord
   belongs_to :shipping_area
   belongs_to :shipping_fee
   belongs_to :shipping_days
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition
+    validates :shipping_fee_id
+    validates :shipping_area_id
+    validates :shipping_days_id
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
+               format: { with: /\A[0-9]+\z/}
+  end
 end
