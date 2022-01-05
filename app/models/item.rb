72aea05edc83d28371eb2e3886f7f55dc2a839b8
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-05 12:22:45 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,4 +23,12 @@ class Item < ApplicationRecord
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
                format: { with: /\A[0-9]+\z/}
   end
+
+  with_options numericality: { other_than: 1 } do
+    validates :condition
+    validates :category
+    validates :shipping_area
+    validates :shipping_fee
+    validates :shipping_days
+  end
 end
