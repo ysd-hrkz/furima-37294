d70d19fcbf4d94324c7725a74bda00d79345fa1e
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-05 14:41:27 +0900

    itemコントローラーnewアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+
+
   private
-  def message_params
+  def item_params
     params.require(:item).permit(:image, :name, :price, :description, :condition_id, :category_id, :shipping_area_id, :shipping_fee_id, :shipping_days_id).merge(user_id: current_user.id)
   end
 end
