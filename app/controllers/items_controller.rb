f59cf2b55e2dbf16d9fbafdad1517ac3fa73e564
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-06 17:43:50 +0900

    Rubocop実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,8 +37,8 @@ class ItemsController < ApplicationController
 
   def destroy
     if @item.user.id == current_user.id
-    @item.destroy
-    redirect_to root_path
+      @item.destroy
+      redirect_to root_path
     end
   end
 
