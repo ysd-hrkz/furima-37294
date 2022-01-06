14652c0fff360759bc1aba2991adde2b6f8d8b20
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-06 12:42:32 +0900

    showアクション実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, except: [:new, :show]
 
   def index
     @items = Item.all.order(id: 'DESC')
@@ -16,6 +16,10 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
