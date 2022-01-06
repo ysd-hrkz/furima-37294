036d1f5c7ce1b6dde3ffc7928ea025c8cb268861
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-06 16:59:25 +0900

    Rubocop実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless @item.user.id == current_user.id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless @item.user.id == current_user.id
   end
 
   def update
