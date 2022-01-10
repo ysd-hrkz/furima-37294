4386fb3cd200884c20e65c03b32a8bf0e846f663
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-10 14:44:52 +0900

    ordersコントローラー、リファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
     @order_shipping = OrderShipping.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @order_shipping = OrderShipping.new(order_params)
-    @item = Item.find(params[:item_id])
     if @order_shipping.valid?
       @order_shipping.save
       return redirect_to root_path
@@ -18,6 +18,10 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:order_shipping).permit(:post_code, :shipping_area_id, :city, :address, :building_name, :phone_num).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
