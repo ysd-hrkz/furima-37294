a439664c35bcdc925f5f866128a0464f8f9f43f7
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-10 14:39:19 +0900

    エラーハンドリング

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -6,11 +6,10 @@ class OrderShipping
     validates :user_id
     validates :item_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :shipping_area_id, numericality: { other_than: 1 , message: "Select"}
     validates :city
     validates :address
-    validates :building_name
-    validates :phone_num, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid"}
+    validates :phone_num, format: {with: /\A[0-9]{10,11}\z/, message: "Input only number"}
   end
 
   def save
