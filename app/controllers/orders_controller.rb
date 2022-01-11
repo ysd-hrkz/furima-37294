065e78b69e8a4f1879bd1474eba173c055064e8a
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-11 11:23:10 +0900

    秘密鍵の環境変数代入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -26,7 +26,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_aecf4495a722d6041c3c03cc"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
         amount: @item.price,
         card: order_params[:token],
