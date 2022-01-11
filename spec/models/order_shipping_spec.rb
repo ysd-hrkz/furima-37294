b05accf9cf9a7db0fc38e86c27638557d4ab6e08
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-11 18:27:21 +0900

    テストコード修正

--- spec/models/order_shipping_spec.rb
+++ spec/models/order_shipping_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe OrderShipping, type: :model do
   before do
-    @order_shipping = FactoryBot.build(:order_shipping)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @order_shipping = FactoryBot.build(:order_shipping, user_id: @user.id, item_id: @item.id)
+    sleep(0.1)
   end
 
   describe '配送先情報の保存' do
