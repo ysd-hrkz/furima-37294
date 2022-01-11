08d5dad3aa9049bf5c5f577b2d8737a186701ac1
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-11 11:25:17 +0900

    JavaScriptで環境変数の呼び込み

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_c643d5745446cf12b7c6fe7b");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
