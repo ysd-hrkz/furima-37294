1ab5374309dc4886163382ad59f096705cc0af32
ysd-hrkz <ysd.hrkz.6344@gmail.com>
2022-01-11 18:41:27 +0900

    テストコードの修正

--- spec/factories/order_shippings.rb
+++ spec/factories/order_shippings.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :order_shipping do
-    user_id { Faker::Number.non_zero_digit }
-    item_id { Faker::Number.non_zero_digit }
     post_code { '123-4567' }
     shipping_area_id { Faker::Number.between(from: 2, to: 48) }
     city { Faker::Address.city }
